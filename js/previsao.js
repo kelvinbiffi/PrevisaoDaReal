77b126a8884ad39a20ee6cf423959a407cd0535b
kelvin.biffi <kelvin.biffi@pmweb.com.br>
2020-01-11 03:33:32 -0300

    Adicionado frases e comportamento de exibição das informações na tela

--- js/previsao.js
+++ js/previsao.js
@@ -7,8 +7,15 @@ class PrevisaoDaReal {
      */
     constructor () {
         this.getIPFromCF();
-        this.city;
-        this.whether;
+        this.cache = {
+            body:  document.querySelector('body'),
+            spinnerContent: document.querySelector('.spinner-content'),
+            mainContent:  document.querySelector('main .content'),
+            sensacao: document.querySelector('.whether-info .sensacao'),
+            temperatura: document.querySelector('.whether-info .temperatura'),
+            humidade: document.querySelector('.whether-info .humidade'),
+            daReal: document.querySelector('.whether-info .da-real'),
+        }
     }
 
     /**
@@ -26,7 +33,6 @@ class PrevisaoDaReal {
         });
     }
     
-
     /**
      * Tries to get data from local storage before calling external API.
      *
@@ -86,11 +92,50 @@ class PrevisaoDaReal {
      * @param {JSON} cityInfo 
      */
     async getWhetherByIP (cityInfo) {
-        const URL = `http://api.openweathermap.org/data/2.5/weather?lat=${cityInfo.geoplugin_latitude}&lon=${cityInfo.geoplugin_longitude}&APPID=3d9543d41cc5c347c206faa9de23d798`;
+        const URL = `http://api.openweathermap.org/data/2.5/weather?lat=${cityInfo.geoplugin_latitude}&lon=${cityInfo.geoplugin_longitude}&APPID=3d9543d41cc5c347c206faa9de23d798&units=metric`;
         const whetherInfo = await this.getDataFromCache(URL, 5);
-        this.city = cityInfo;
-        this.whether = whetherInfo;
-        console.log(this);
+        this.getTerms(cityInfo, whetherInfo);
+    }
+
+    /**
+     * Get avatar for the whether
+     * 
+     * @param {JSON} city 
+     * @param {JSON} whether 
+     */
+    async getTerms (city, whether) {
+        console.log(city,whether);
+        const terms = await this.getDataFromCache('js/previsao.json', 1);
+        console.log(terms);
+
+        this.cache.sensacao.textContent = `Sensação térmica: ${whether.main.feels_like}`;
+        this.cache.temperatura.textContent = `Mínima: ${whether.main.temp_min} °C, Máxima: ${whether.main.temp_max} °C`;
+        this.cache.humidade.textContent = `Humidade ${whether.main.humidity}%`;
+
+        let avatar, frase;
+        if (whether.main.feels_like < 12) {
+            avatar = terms.avatar.frio;
+            frase = terms.frase.frio;
+        } else if (whether.main.feels_like >= 12 && whether.main.feels_like < 22) {
+            avatar = terms.avatar.fresco;
+            frase = terms.frase.fresco;
+        } else {
+            avatar = terms.avatar.calor;
+            frase = terms.frase.calor;
+        }
+
+        this.cache.daReal.textContent = '"'+frase[Math.floor(Math.random()*frase.length)]+'"';
+        avatar = avatar[Math.floor(Math.random()*avatar.length)];
+        const img = document.createElement('img');
+        img.src = avatar.image;
+        this.cache.body.style.backgroundColor = avatar.color;
+        this.cache.mainContent.insertAdjacentElement('afterBegin', img);
+
+        this.cache.spinnerContent.classList.add('hide');
+    }
+
+    showInfoScreen () {
+
     }
 
 }
